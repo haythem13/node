310de89b70bdc28b29d7774fe3658f1c3f5e90fa
haythemHamdi <35465738+haythem13@users.noreply.github.com>
2018-04-03 16:18:16 +0100

    new changes

--- app01.js
+++ app01.js
@@ -2,10 +2,10 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
- // parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
  
-// parse application/json
+
 app.use(bodyParser.json())
 app.get('/', function (req, res) {
   res.send('Hello World ')
